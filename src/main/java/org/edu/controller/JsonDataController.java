a7ca8cd1c37d51f1590189a8e50e67b3bd2b1577
oksk3450@naver.com <oksk3450@naver.com>
2021-04-19 10:09:12 +0900

    ㅌㅅㅌ

--- src/main/java/org/edu/controller/JsonDataController.java
+++ src/main/java/org/edu/controller/JsonDataController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.edu.dao.IF_MemberDAO;
+import org.edu.vo.ChartVO;
 import org.edu.vo.MemberVO;
 import org.edu.vo.PageVO;
 import org.hsqldb.lib.SimpleLog;
@@ -13,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +26,48 @@ public class JsonDataController {
 	private IF_MemberDAO memberDAO;
 	private Logger logger = Logger.getLogger(SimpleLog.class);
 	
+	//RestAPI서버 : 오픈챠트jsp에 투표한 값을 삭제
+	@RequestMapping(value="/chart/deldata", method=RequestMethod.POST)
+		public ResponseEntity<String> delData() {
+			ResponseEntity<String> entity = null;
+			try {
+				memberDAO.delData();
+				entity = new ResponseEntity<>("success", HttpStatus.OK);
+			}catch(Exception e) {
+				entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			}
+			return entity;		
+		}
+	//RestAPI서버 : 오픈챠트js에 투표한 값을 저장(인서트|업데이트 합니다.)
+		@RequestMapping(value="/chart/setdata", method=RequestMethod.POST)
+		public ResponseEntity<String> setData(@RequestBody ChartVO chartVO) {
+			ResponseEntity<String> entity = null;
+			try {
+				logger.debug("전송값 확인"+chartVO.toString());
+				ChartVO countVO = memberDAO.getData();
+				if(countVO == null) {
+					memberDAO.insertData(chartVO);
+				}else {
+					memberDAO.updateData(chartVO);
+				}
+				entity = new ResponseEntity<>("success", HttpStatus.OK);
+			}catch(Exception e) {
+				entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			}
+			return entity;
+		}
+	//RestAPI서버 : 오픈챠트js에서 기존 투표한 자료 가져오기
+	@RequestMapping(value="/chart/getdata", method=RequestMethod.GET)
+	public ResponseEntity<ChartVO> getData() {
+		ResponseEntity<ChartVO> entity = null;
+		try {
+			ChartVO chartVO = memberDAO.getData();
+			entity = new ResponseEntity<>(chartVO, HttpStatus.OK);//200
+		}catch(Exception e) {
+			entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);//400
+		}
+		return entity;
+	}
 	//RestAPI인증서버 : 안드로이드앱에서 회원목록중 선택한 id 삭제.
 	@RequestMapping(value="/android/delete/{user_id}", method=RequestMethod.POST)
 	public ResponseEntity<String> androidDelete(@PathVariable("user_id") String user_id) {
